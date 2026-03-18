3d9a885f39308669b43c237ce6a6d92024754cbb
rochamrcs <mavinicius95@gmail.com>
2026-03-18 13:04:13 -0300

    feat:configure product admin with list_display, search and filters

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['product_code', 'name', 'umb', 'status', 'product_type', 'quantity', 'created_at']
+    search_fields = ['product_code', 'name']
+    list_filter = ['status', 'umb', 'product_type']
+
+admin.site.register(Product, ProductAdmin)
