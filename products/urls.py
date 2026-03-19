20274153b28d25980e8e90534c60415020c6e789
rochamrcs <mavinicius95@gmail.com>
2026-03-19 20:34:59 -0300

    feat(product): add list and create pages with forms and views

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import products
+from .views import products, new_product
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', products),
+    path('', products, name='products'),
+    path('new_product/', view=new_product, name='new_product')
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from products.models import Product
+from products.forms import ProductForm
 
 def products(request):
-    return render(request, 'products.html')
+    search = request.GET.get('q')
+    products_list = Product.objects.all()
+
+    if search:
+        products_list = Product.objects.filter(name__icontains=search)
+
+    return render(request, 'products.html', {"products_list": products_list})
+
+def new_product(request):
+    if request.method == 'POST':
+        new_product_form = ProductForm(request.POST)
+        print(new_product_form)
+
+        if new_product_form.is_valid():
+            new_product_form.save()
+            return redirect('products:products')
+    else:
+        new_product_form = ProductForm()
+    return render(request, 'new_product.html', {'new_product': new_product_form})
