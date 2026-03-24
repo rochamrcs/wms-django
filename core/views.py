8edfbf5178473e3a4e39de9278ecba223e058c87
rochamrcs <mavinicius95@gmail.com>
2026-03-24 19:38:33 -0300

    feat(core): add login required

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='accounts:login_form')
 def home_page(request):
     return render(request, 'base.html')
