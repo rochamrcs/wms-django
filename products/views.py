f13044e13778cfd66bf65f1af4fe953095b9d9a3
rochamrcs <mavinicius95@gmail.com>
2026-03-31 18:34:15 -0300

    fix(products): Include the correct importation for login required

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from products.models import Product
 from products.forms import ProductForm
+from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='accounts:login_form')
 def products(request):
