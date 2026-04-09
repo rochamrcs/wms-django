708be3b38f4dfa70fd2badb5225384f6fc31e45b
rochamrcs <mavinicius95@gmail.com>
2026-04-08 22:30:48 -0300

    fix(product): fix Q import

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from products.models import Product
 from products.forms import ProductForm
+from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='accounts:login_form')
